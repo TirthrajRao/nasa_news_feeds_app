f228d8bc5c020ae27fd471ab39519519dfe740b2
tirthraj rao <tirthrajrao2394@gmail.com>
2019-05-31 20:14:13 +0530

    Updates

---
+++ precache-manifest.9eb9f23702ea140b20c9225dd9f96531.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c9157dce8d54e6cfcca9622e1408d22",
+    "url": "/nasa_news_feeds_app/index.html"
+  },
+  {
+    "revision": "9c2737ea7985b298a1e6",
+    "url": "/nasa_news_feeds_app/static/css/2.428527f0.chunk.css"
+  },
+  {
+    "revision": "8324b1b70bcd7a78b0f4",
+    "url": "/nasa_news_feeds_app/static/css/main.6f0012f1.chunk.css"
+  },
+  {
+    "revision": "9c2737ea7985b298a1e6",
+    "url": "/nasa_news_feeds_app/static/js/2.c44cf31d.chunk.js"
+  },
+  {
+    "revision": "8324b1b70bcd7a78b0f4",
+    "url": "/nasa_news_feeds_app/static/js/main.df38b7a8.chunk.js"
+  },
+  {
+    "revision": "7c4e700186a46a57dff5",
+    "url": "/nasa_news_feeds_app/static/js/runtime~main.a019a25f.js"
+  },
+  {
+    "revision": "aa9ad39431a36af0c2f8a6ee97ab1d9d",
+    "url": "/nasa_news_feeds_app/static/media/cneos_logo.aa9ad394.png"
+  },
+  {
+    "revision": "eafbc460207552c8c9eb89214a691287",
+    "url": "/nasa_news_feeds_app/static/media/earth2.eafbc460.png"
+  },
+  {
+    "revision": "9d7a50f214c64cf2d8702cb73fe1c46d",
+    "url": "/nasa_news_feeds_app/static/media/facebook.9d7a50f2.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/nasa_news_feeds_app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/nasa_news_feeds_app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/nasa_news_feeds_app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/nasa_news_feeds_app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/nasa_news_feeds_app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "751a308e301c42b19e6706d859bc0abb",
+    "url": "/nasa_news_feeds_app/static/media/instagram.751a308e.png"
+  },
+  {
+    "revision": "80115d2cb996a9496e06db2e2e81c272",
+    "url": "/nasa_news_feeds_app/static/media/nasa-logo.80115d2c.svg"
+  }
+]);
